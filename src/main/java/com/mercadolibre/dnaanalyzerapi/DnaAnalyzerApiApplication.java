7433f4704cb3dffc713265bb8534d29b1f273d86
Raymundo Diaz Vega <rvega@excelcredit.co>
2022-03-27 19:00:08 -0500

    project added

---
+++ src/main/java/com/mercadolibre/dnaanalyzerapi/DnaAnalyzerApiApplication.java
@@ -0,0 +1,12 @@
+package com.mercadolibre.dnaanalyzerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnaAnalyzerApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DnaAnalyzerApiApplication.class, args);
+  }
+}
