f6845374c27778cebb1a34b64f02aefaf3c23015
Raymundo Diaz Vega <rvega@excelcredit.co>
2022-04-26 22:06:54 -0500

    Improvements

--- src/main/java/com/mercadolibre/dnaanalyzerapi/stats/application/StatsService.java
+++ src/main/java/com/mercadolibre/dnaanalyzerapi/stats/application/StatsService.java
@@ -2,11 +2,9 @@ package com.mercadolibre.dnaanalyzerapi.stats.application;
 
 import com.mercadolibre.dnaanalyzerapi.human.infrastructure.ports.HumanRepository;
 import com.mercadolibre.dnaanalyzerapi.stats.dto.Stats;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Slf4j
 @Service
 public class StatsService {
 
